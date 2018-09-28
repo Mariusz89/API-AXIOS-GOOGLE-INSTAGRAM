c3d520a598325e0c42bd64f469af679483359d34
mariusz <mariusz.kosior@onet.pl>
2018-09-29 00:29:20 +0200

    Add new component NoMovies

---
+++ appinstagram/src/NoMovies.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NoMovies = props => (
+  <li className='no-movies'>
+    <i className="material-icons icon-gif">sentiment_very_dissatisfied</i>
+    <h3>Sorry, no MOVIES match your search.</h3>
+  </li>
+);
+
+export default NoMovies;
